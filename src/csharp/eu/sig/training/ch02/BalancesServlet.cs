1a1f4470ee9ef0bb6b5df79153ff28615a511948
Valentina Hekimova <vhekimova@vhekimovalaptop.sig.eu>
2020-01-27 11:58:23 +0100

    refactored DoGet method

--- src/csharp/eu/sig/training/ch02/BalancesServlet.cs
+++ src/csharp/eu/sig/training/ch02/BalancesServlet.cs
@@ -12,19 +12,42 @@ namespace eu.sig.training.ch02
         // security flaws. However, that's beside the point for this example.
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2100:Review SQL queries for security vulnerabilities")]
         // tag::doGet[]
+        //handling HTTP request
+        //DB access
+        //business logic
+        //formatting data (JSON)
         public void DoGet(HttpRequest req, HttpResponse resp)
         {
             resp.ContentType = "application/json";
+            var dataAdapter = GetAdapter(req);
+            var dataTable = GetAccountData(dataAdapter);
+            try
+            {
+                CheckAccount(dataTable,resp);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"SQL exception: {e.Message}");
+            }
+        }
+        // end::doGet[]
+        private SqlDataAdapter GetAdapter(HttpRequest req)
+        {
             string command = "SELECT account, balance " +
                 "FROM ACCTS WHERE id=" + req.Params[
                     ConfigurationManager.AppSettings["request.parametername"]];
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(command,
+            return new SqlDataAdapter(command,
                 ConfigurationManager.AppSettings["handler.serverstring"]);
+        }
+        private DataTable GetAccountData(SqlDataAdapter dataAdapter)
+        {
             DataSet dataSet = new DataSet();
             dataAdapter.Fill(dataSet, "ACCTS");
             DataTable dataTable = dataSet.Tables[0];
-            try
-            {
+            return dataTable;
+        }
+         private void CheckAccount(DataTable dataTable, HttpResponse resp)
+         {
                 float totalBalance = 0;
                 int rowNum = 0;
                 resp.Write("{\"balances\":[");
@@ -32,19 +55,8 @@ namespace eu.sig.training.ch02
                 {
                     rowNum++;
                     DataRow results = (DataRow)dataTable.Rows.GetEnumerator().Current;
-                    // Assuming result is 9-digit bank account number,
-                    // validate with 11-test:
-                    int sum = 0;
-                    for (int i = 0; i < ((string)results["account"]).Length; i++)
-                    {
-                        sum = sum + (9 - i) *
-                            (int)Char.GetNumericValue(((string)results["account"])[i]);
-                    }
-                    if (sum % 11 == 0)
-                    {
-                        totalBalance += (float)results["balance"];
-                        resp.Write($"{{\"{results["account"]}\":{results["balance"]}}}");
-                    }
+                    CheckAccountLength(results, totalBalance, resp);
+                    
                     if (rowNum == dataTable.Rows.Count)
                     {
                         resp.Write("],\n");
@@ -55,12 +67,22 @@ namespace eu.sig.training.ch02
                     }
                 }
                 resp.Write($"\"total\":{totalBalance}}}\n");
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine($"SQL exception: {e.Message}");
-            }
-        }
-        // end::doGet[]
-    }
+         }
+             // Assuming result is 9-digit bank account number,
+            // validate with 11-test:
+         private void CheckAccountLength(DataRow results, float totalBalance, HttpResponse resp)
+         {
+             int sum = 0;
+                    for (int i = 0; i < ((string)results["account"]).Length; i++)
+                    {
+                        sum = sum + (9 - i) *
+                            (int)Char.GetNumericValue(((string)results["account"])[i]);
+                    }
+                    if (sum % 11 == 0)
+                    {
+                        totalBalance += (float)results["balance"];
+                        resp.Write($"{{\"{results["account"]}\":{results["balance"]}}}");
+                    }
+         }
+    } 
 }
